705f3654cb80518fd5c7c7067792cec917e5ffba
Yike Gao <yikegao8@gmail.com>
2022-09-22 13:07:27 +0100

    9.22

--- src/components/current-weather/current-weather.js
+++ src/components/current-weather/current-weather.js
@@ -10,7 +10,32 @@ const CurrentWeather = ()=>{
         </div>
         <img src="pictures/01.png" alt="weather" className="weather-icon" />
       </div>
-  
+  <div className="bottom">
+    <p className="temperature">
+        18°C
+    </p>
+    <div className="details">
+        <div className="parameter-row">
+            <span className="parameter-label">Details</span>
+        </div>
+        <div className="parameter-row">
+            <span className="parameter-label">Feels like</span>
+            <span className="parameter-value"> 18°C</span>
+        </div>
+        <div className="parameter-row">
+            <span className="parameter-label">Wind</span>
+            <span className="parameter-value"> 2 m/s</span>
+        </div>
+        <div className="parameter-row">
+            <span className="parameter-label">Humidity</span>
+            <span className="parameter-value">15%</span>
+        </div>
+        <div className="parameter-row">
+            <span className="parameter-label">Pressure</span>
+            <span className="parameter-value">15 hPa</span>
+        </div>
+    </div>
+  </div>
     </div>
     );
 }
